4cd3bd0309851da53e6cc0c00eff99a699f55458
hyunjkim <hkim6501@gmail.com>
2016-12-10 17:46:56 -0500

    FifthStep

--- app/src/main/java/nyc/c4q/hyun/vine_app/VineViewHolder.java
+++ app/src/main/java/nyc/c4q/hyun/vine_app/VineViewHolder.java
@@ -1,7 +1,9 @@
 package nyc.c4q.hyun.vine_app;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import nyc.c4q.hyun.vine_app.network.Record;
@@ -12,13 +14,20 @@ import nyc.c4q.hyun.vine_app.network.Record;
 public class VineViewHolder extends RecyclerView.ViewHolder {
 
     private TextView usernameTV;
+    private TextView likedTV;
+    private LinearLayout textBox;
 
     public VineViewHolder(View itemView) {
         super(itemView);
         usernameTV = (TextView) itemView.findViewById(R.id.vineusername_TV);
+        likedTV = (TextView) itemView.findViewById(R.id.vineliked_TV);
+        textBox = (LinearLayout) itemView.findViewById(R.id.vinetextbox_background);
     }
 
     public void setData(Record record) {
+        textBox.setBackground(Drawable.createFromPath(record.getProfileBackground()));
         usernameTV.setText(record.getUsername());
+        likedTV.setText(record.getLiked());
+
     }
 }
